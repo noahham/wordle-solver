eee7ca4314c266413b3320ad302d61cace1dcc43
Noah Ham <68747182+noahham@users.noreply.github.com>
2024-11-19 19:17:04 -0800

    Rewrite main.py

--- main.py
+++ main.py
@@ -1,279 +1,190 @@
-# IMPORTS
-from cgitb import text
+'''
+Noah Ham
+11/19/24
+Wordle Solver 1.3
+'''
+
 from tkinter import *
 from tkinter import ttk
 
-# Setting up 'words' list
-wordsTemp1 = open("words.txt", "r")
-wordsTemp2 = wordsTemp1.readlines()
-words = []
-for line in wordsTemp2:
-    words.append(line.strip())
-words.sort()
+class Solver:
+    def __init__(self):
+        """ Initializes the app and necessary variables """
+        self.root = Tk()
+        self.setupTk()
+        
+        self.setupWordsList('words.txt')
 
-filterWords = words
+        # Instantiates dropdown variables
+        self.letter = StringVar()
+        self.confirm = StringVar()
+        self.position = StringVar()
+        self.letter.set("A")
+        self.confirm.set("is on column")
+        self.position.set("1")
 
-concFilterWords = []
+        self.posOptions = ['1', '2', '3', '4', '5']
 
-# DISPLAY
-root = Tk()
+        self.createElements()
+        self.reset()
 
-# Variables
-letter = StringVar()
-letter.set("A")
+    def setupTk(self):
+        """ Initializes Tk object """
+        self.root.geometry("350x400")
+        self.root.configure(bg="#21252b")
+        self.root.title("Wordle Solver")
 
-confirm = StringVar()
-confirm.set("is on column")
+        style = ttk.Style()
+        style.theme_use("classic")
+        style.configure("TCombobox", fieldbackground="#272b32", background="#21252b", foreground="white", borderwidth=0,
+                        justify="center")
+        style.configure("TFrame", fieldbackground="#272b32", background="#21252b", foreground="white", borderwidth=0,
+                        justify="center")
 
-position = StringVar()
-position.set("1")
+    def setupWordsList(self, filename):
+        """
+        Takes the file and puts the words from it in a sorted list
 
-posOptions = [1, 2, 3, 4, 5]
+        Parameters:
+            filename (string): the name of the file
 
-# Functions
+        """
+        self.words = []
 
-def addFilter():
-# Letter 1
-    if position.get() == "1":
-    # UI
-        posOptions.remove(1)
-        posInput.config(values=posOptions)
-        letter_1.config(text=letter.get())
-        if confirm.get() == "is in column":
-            letterBG_1.config(bg="#558d4e")
-            letter_1.config(bg="#558d4e")
-        else:
-            letterBG_1.config(bg="#b59f3b")
-            letter_1.config(bg="#b59f3b")
-    # List
-        if confirm.get() == "is in column":
+        wordsTemp1 = open(filename, "r")
+        wordsTemp2 = wordsTemp1.readlines()
+
+        for line in wordsTemp2:
+            self.words.append(line.strip())
+        self.words.sort()
+
+    def createElements(self):
+        """ Creates all tkinter elements for the app """
+
+        # Dropdowns
+
+        self.letterInput = ttk.Combobox(self.root, textvariable=self.letter, width=2, font=("Arial Black", 14), justify="center",
+                                        values=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+                                        "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
+
+        self.letterInput.place(relx=.175, rely=.075, anchor="center")
+
+        self.colInput = ttk.Combobox(self.root, textvariable=self.confirm, width=13, font=("Arial Bold", 14), justify="center",
+                                values=["is in column", "isn't in column"])
+
+        self.colInput.place(relx=.5, rely=.075, anchor="center")
+
+        self.posInput = ttk.Combobox(self.root, textvariable=self.position, state="", width=2, font=("Arial Black", 14), justify="center",
+                                values=self.posOptions)
+        self.posInput.place(relx=.825, rely=.075, anchor="center")
+
+        # Buttons
+
+        addButton = Button(self.root, text="Add", width=9, font=("Arial Bold", 12),
+                            bd=0, fg="white", bg="#272b32", activeforeground="green", activebackground="green",
+                            command=lambda: self.addFilter(int(self.position.get()) - 1, (self.letter.get()).lower()))
+        addButton.place(relx=.35, rely=.175, anchor="center")
+
+        resetButton = Button(self.root, text="Reset", width=9, font=("Arial Bold", 12),
+                            bd=0, fg="white", bg="#272b32", activeforeground="#272b32", activebackground="#2a3037",
+                            command=self.reset)
+        resetButton.place(relx=.65, rely=.175, anchor="center")
+
+        # Letters
+
+        self.letters = []
+        self.lettersBG = []
+        xPos = .18
+
+        for i in range(5):
+            self.lettersBG.append(Canvas(self.root, width=45, height=45, bg="#272b32", highlightthickness=0))
+            self.letters.append(Label(self.root, text="?", fg="white", bg="#272b32", font=("Arial Black", 20)))
+
+            self.lettersBG[i].place(relx=xPos, rely=.31, anchor="center")
+            self.letters[i].place(relx=xPos, rely=.31, anchor="center")
+            xPos += .16
+
+        # Possible Answers
+
+        self.output = Text(self.root, state="disabled", wrap="word", height=11, width=35, fg="white", bg="#272b32",
+                            font=("Arial Bold", 12))
+        self.output.place(relx=.5, rely=.68, anchor="center")
+
+    def addFilter(self, posIndex, letterToFind):
+        """
+        Adds a hint that filters the possible words that the answer could be
+
+        Parameters
+            (int) posIndex: The index (0-4) of the new filter
+            (char) letterToFind: The letter to find in the new filter
+
+        """
+
+        self.letters[posIndex].config(text=self.letter.get())
+
+        if self.confirm.get() == "is in column":
+            # Updates position dropdown
+            self.posOptions.remove(str(posIndex + 1))
+            self.posInput.config(values=self.posOptions)
+
+            # Changes letter and BG to green
+            self.lettersBG[posIndex].config(bg="#558d4e")
+            self.letters[posIndex].config(bg="#558d4e")
+
+            # Removes all words that don't have the letter in right spot
             i = 0
-            while i < len(filterWords):
-                if (filterWords[i])[0:1] != (letter.get()).lower():
-                    filterWords.remove(filterWords[i])
-                    i -= 1
+            while i < len(self.words):
+                if self.words[i][posIndex] != letterToFind:
+                    self.words.remove(self.words[i])
+                    i -= 1 # Goes back after removing it
                 i += 1
         else:
-            posIndex = int(position.get()) -1
+            # Changes letter and BG to yellow
+            self.lettersBG[posIndex].config(bg="#b59f3b")
+            self.letters[posIndex].config(bg="#b59f3b")
+
+            # Removes all words that have the letter in the right spot or don't have the letter at all
             i = 0
-            while i < len(filterWords):
-                if filterWords[i].find((letter.get()).lower()) == -1 or filterWords[i].find((letter.get()).lower()) == posIndex:
-                    filterWords.remove(filterWords[i])
-                    i -= 1
+            while i < len(self.words):
+                if self.words[i].find(letterToFind) == -1 or self.words[i][posIndex] == letterToFind:
+                    self.words.remove(self.words[i])
+                    i -= 1  # Goes back after removing it
                 i += 1
-# Letter 2
-    elif position.get() == "2":
-    # UI
-        posOptions.remove(2)
-        posInput.config(values=posOptions)
-        letter_2.config(text=letter.get())
-        if confirm.get() == "is in column":
-            letterBG_2.config(bg="#558d4e")
-            letter_2.config(bg="#558d4e")
-        else:
-            letterBG_2.config(bg="#b59f3b")
-            letter_2.config(bg="#b59f3b")
-    # List
-        if confirm.get() == "is in column":
-            i = 0
-            while i < len(filterWords):
-                if (filterWords[i])[1:2] != (letter.get()).lower():
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-        else:
-            posIndex = int(position.get()) -1
-            i = 0
-            while i < len(filterWords):
-                if filterWords[i].find((letter.get()).lower()) == -1 or filterWords[i].find((letter.get()).lower()) == posIndex:
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-# Letter 3
-    elif position.get() == "3":
-    # UI
-        posOptions.remove(3)
-        posInput.config(values=posOptions)
-        letter_3.config(text=letter.get())
-        if confirm.get() == "is in column":
-            letterBG_3.config(bg="#558d4e")
-            letter_3.config(bg="#558d4e")
-            
-            
-        else:
-            letterBG_3.config(bg="#b59f3b")
-            letter_3.config(bg="#b59f3b")
-    # List
-        if confirm.get() == "is in column":
-            i = 0
-            while i < len(filterWords):
-                if (filterWords[i])[2:3] != (letter.get()).lower():
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-        else:
-            posIndex = int(position.get()) -1
-            i = 0
-            while i < len(filterWords):
-                if filterWords[i].find((letter.get()).lower()) == -1 or filterWords[i].find((letter.get()).lower()) == posIndex:
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-# Letter 4
-    elif position.get() == "4":
-    # UI
-        posOptions.remove(4)
-        posInput.config(values=posOptions)
-        letter_4.config(text=letter.get())
-        if confirm.get() == "is in column":
-            letterBG_4.config(bg="#558d4e")
-            letter_4.config(bg="#558d4e")
-        else:
-            letterBG_4.config(bg="#b59f3b")
-            letter_4.config(bg="#b59f3b")
-    # List
-        if confirm.get() == "is in column":
-            i = 0
-            while i < len(filterWords):
-                if (filterWords[i])[3:4] != (letter.get()).lower():
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-        else:
-            posIndex = int(position.get()) -1
-            i = 0
-            while i < len(filterWords):
-                if filterWords[i].find((letter.get()).lower()) == -1 or filterWords[i].find((letter.get()).lower()) == posIndex:
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-# Letter 5
-    else:
-    # UI
-        posOptions.remove(5)
-        posInput.config(values=posOptions)
-        letter_5.config(text=letter.get())
-        if confirm.get() == "is in column":
-            letterBG_5.config(bg="#558d4e")
-            letter_5.config(bg="#558d4e")
-        else:
-            letterBG_5.config(bg="#b59f3b")
-            letter_5.config(bg="#b59f3b")
-    # List
-        if confirm.get() == "is in column":
-            i = 0
-            while i < len(filterWords):
-                if (filterWords[i])[4:5] != (letter.get()).lower():
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-        else:
-            posIndex = int(position.get()) -1
-            i = 0
-            while i < len(filterWords):
-                if filterWords[i].find((letter.get()).lower()) == -1 or filterWords[i].find((letter.get()).lower()) == posIndex:
-                    filterWords.remove(filterWords[i])
-                    i -= 1
-                i += 1
-    concFilterWords = (", ".join(filterWords))
-    output.config(state="normal")
-    output.delete("1.0","end")
-    output.insert(INSERT, (concFilterWords))
-    output.config(state="disabled")
-    print("Filter added.")
 
+        # Refreshes dropdowns
+        self.letterInput.current(0)
+        self.posInput.current(0)
 
-def resetFilter():
-    letter_1.config(text="?", bg="#272b32")
-    letter_2.config(text="?", bg="#272b32")
-    letter_3.config(text="?", bg="#272b32")
-    letter_4.config(text="?", bg="#272b32")
-    letter_5.config(text="?", bg="#272b32")
-    
-    letterBG_1.config(bg="#272b32")
-    letterBG_2.config(bg="#272b32")
-    letterBG_3.config(bg="#272b32")
-    letterBG_4.config(bg="#272b32")
-    letterBG_5.config(bg="#272b32")
-    
-    filterWords = words
-    posOptions = [1, 2, 3, 4, 5]
-    posInput.config(values=posOptions)
-    
-    output.config(state="normal")
-    output.delete("1.0","end")
-    output.config(state="disabled")
-    print("Filters cleared.")
-    
+        # Refreshes output box
+        self.output.config(state="normal")
+        self.output.delete("1.0", "end")
+        self.output.insert(INSERT, (", ".join(self.words)))
+        self.output.config(state="disabled") # User can't type in it
 
-# Screen
-root.geometry("350x400")
-root.configure(bg="#21252b")
-root.title("Wordle Solver")
+    def reset(self):
+        """ Resets the entire program to start from scratch """
 
-style = ttk.Style()
-style.theme_use("classic")
-style.configure("TCombobox", fieldbackground="#272b32", background="#21252b", foreground="white", borderwidth=0, justify="center")
-style.configure("TFrame", fieldbackground="#272b32", background="#21252b", foreground="white", borderwidth=0, justify="center")
+        for char in self.letters:
+            char.config(text="?", bg="#272b32")
 
+        for char in self.lettersBG:
+            char.config(bg="#272b32")
 
-# Elements
-letterInput = ttk.Combobox(root, textvariable=letter, width=2, font=("Arial Black", 14), justify="center",
-                           values=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
-letterInput.current(0)
-letterInput.place(relx=.175, rely=.075, anchor="center")
+        self.setupWordsList("words.txt")
+        self.posOptions = ['1', '2', '3', '4', '5']
+        self.posInput.config(values=self.posOptions)
 
-conInput = ttk.Combobox(root, textvariable=confirm, width=13, font=("Arial Bold", 14), justify="center", 
-                        values=["is in column", "isn't in column"])
-conInput.current(0)
-conInput.place(relx=.5, rely=.075, anchor="center")
+        self.output.config(state="normal")
+        self.output.delete("1.0", "end")
+        self.output.config(state="disabled")
 
-posInput = ttk.Combobox(root, textvariable=position, state="", width=2, font=("Arial Black", 14), justify="center", 
-                        values=posOptions)
-posInput.current(0)
-posInput.place(relx=.825, rely=.075, anchor="center")
+        self.letterInput.current(0)
+        self.posInput.current(0)
+        self.colInput.current(0)
 
-addButton = Button(root, text="Add", width=9, font=("Arial Bold", 12), 
-                    bd=0, fg="white", bg="#272b32", activeforeground="white", activebackground="#2a3037", command=addFilter)
-addButton.place(relx=.35, rely=.175, anchor="center")
+def main():
+    s = Solver()
+    s.root.mainloop()
 
-resetButton = Button(root, text="Reset", width=9, font=("Arial Bold", 12), 
-                    bd=0, fg="white", bg="#272b32", activeforeground="white", activebackground="#2a3037", command=resetFilter)
-resetButton.place(relx=.65, rely=.175, anchor="center")
-
-
-letterBG_1 = Canvas(root, width=45, height=45, bg="#272b32", highlightthickness=0)
-letterBG_1.place(relx=.18, rely=.31, anchor="center")
-
-letterBG_2 = Canvas(root, width=45, height=45, bg="#272b32", highlightthickness=0)
-letterBG_2.place(relx=.34, rely=.31, anchor="center")
-
-letterBG_3 = Canvas(root, width=45, height=45, bg="#272b32", highlightthickness=0)
-letterBG_3.place(relx=.5, rely=.31, anchor="center")
-
-letterBG_4 = Canvas(root, width=45, height=45, bg="#272b32", highlightthickness=0)
-letterBG_4.place(relx=.66, rely=.31, anchor="center")
-
-letterBG_5 = Canvas(root, width=45, height=45, bg="#272b32", highlightthickness=0)
-letterBG_5.place(relx=.82, rely=.31, anchor="center")
-
-
-letter_1 = Label(root, text="?",fg="white", bg="#272b32", font=("Arial Black", 20))
-letter_1.place(relx=.18, rely=.31, anchor="center")
-
-letter_2 = Label(root, text="?",fg="white", bg="#272b32", font=("Arial Black", 20))
-letter_2.place(relx=.34, rely=.31, anchor="center")
-
-letter_3 = Label(root, text="?",fg="white", bg="#272b32", font=("Arial Black", 20))
-letter_3.place(relx=.5, rely=.31, anchor="center")
-
-letter_4 = Label(root, text="?",fg="white", bg="#272b32", font=("Arial Black", 20))
-letter_4.place(relx=.66, rely=.31, anchor="center")
-
-letter_5 = Label(root, text="?",fg="white", bg="#272b32", font=("Arial Black", 20))
-letter_5.place(relx=.82, rely=.31, anchor="center")
-
-output = Text(root, state="disabled", wrap="word", height=11, width=35, fg = "white", bg="#272b32", font=("Arial Bold", 12))
-output.place(relx=.5, rely=.68, anchor="center")
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
